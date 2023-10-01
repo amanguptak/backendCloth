0110d6f455212f90862d95b20ae8aad3c60b19f1
amanguptak <imaman9294@gmail.com>
2023-10-01 12:37:24 +0530

    header-added1

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,9 @@ connectDB()
 
 app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Origin','*')
-  res.header('Access-Control-Allow-Headers'
-  ,'Origin, X-Requested-With,Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods'
+  ,'GET, POST, PUT,DELETE ');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type')
   next();
 })
 app.listen(process.env.PORT,()=>{
