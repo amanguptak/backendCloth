587228e578c239a42f1d90fb98a9c3fc8ee6ff1c
amanguptak <imaman9294@gmail.com>
2023-10-01 12:43:20 +0530

    header-added

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB()
 
 
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*')
+  res.setHeader('Access-Control-Allow-Origin','http://localhost:8080')
   res.setHeader('Access-Control-Allow-Methods'
   ,'GET, POST, PUT,DELETE ');
   res.setHeader('Access-Control-Allow-Methods','Content-Type')
